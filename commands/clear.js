51ff911c8ac869e724ddfb15143c67608330284a
rohank05 <33832145+rohank05@users.noreply.github.com>
2020-06-08 13:21:55 +0530

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,14 +1,10 @@
 module.exports.run = async(client, message, args, con)=>{
-    if(!args) return message.channel.send("Please enter the command in correct order ```clear <message title>```")
-    var finalString = '';
-    for(var i = 0; i<args.length;i++){
-        finalString += args[i]
-        if(args[i+1]){
-            finalString += ' '
-        }
-    }
+    if(!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send("You don't have correct permission")
+
+    if(!args.length) return message.channel.send("Please enter the command in correct order ```clear <message id>```")
     
-    con.query(`DELETE FROM reactrole WHERE title='${finalString}'`,(err)=>{
+    
+    con.query(`DELETE FROM reactrole WHERE messageid='${args[0]}'`,(err)=>{
         if(err) return(message.channel.send("Please insert the correct title"))
         else{
             return(message.channel.send("Please delete the message. The roles has been cleared"))
